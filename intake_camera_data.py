b1bdfbff95d6fa0664eda1446cbe16ec0693e106
Will Rathgeb <willrathgeb@berkeley.edu>
2023-10-29 04:03:51 -0700

    first draft intake

---
+++ intake_camera_data.py
@@ -0,0 +1,82 @@
+import os
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+import groundingdino.datasets.transforms as T
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+
+
+HOME = os.getcwd()
+# set model configuration file path
+# CONFIG_PATH = os.path.join(HOME, "groundingdino/config/GroundingDINO_SwinT_OGC.py")
+
+# set model weight file ath
+WEIGHTS_PATH = 'GroundingDINO/weights/groundingdino_swint_ogc.pth'
+
+# set text prompt
+TEXT_PROMPT = "glass with lid"
+
+# set box and text threshold values
+BOX_TRESHOLD = 0.35
+TEXT_TRESHOLD = 0.25
+PREDICTION_THRESHOLD = 0.75
+
+model = load_model("GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "GroundingDINO/weights/groundingdino_swint_ogc.pth")
+
+item_not_found = True
+
+while item_not_found:
+     # ESP32 URL
+    URL = 'http://172.20.10.6'
+    AWB = True
+
+    # Face recognition and opencv setup
+    cap = cv2.VideoCapture(URL + ":81/stream")
+    
+    ret, frame = cap.read()
+    # create a transform function by applying 3 image transaformations
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]
+    )
+    # convert frame to a PIL object in RGB space
+    image_source = Image.fromarray(frame).convert("RGB")
+    # convert the PIL image object to a transform object
+    image_transformed, _ = transform(image_source, None)
+    
+    image_source.save('test.jpg')
+
+    # predict boxes, logits, phrases
+    boxes, logits, phrases = predict(
+    model=model, 
+    image=image_transformed, 
+    caption=TEXT_PROMPT, 
+    box_threshold=BOX_TRESHOLD, 
+    text_threshold=TEXT_TRESHOLD,
+    device='cpu')
+    
+    # annotate the image
+    annotated_frame = annotate(image_source=frame, boxes=boxes, logits=logits, phrases=phrases)
+    # display the output
+    out_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+    cv2.imshow('frame', out_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    if logits >= PREDICTION_THRESHOLD:
+        item_not_found = False
+        print("Item found!")
+        print("Item is: " + phrases)
+        print("Confidence: " + str(logits))
+        print("Box: " + str(boxes))
+        print("Text: " + str(phrases))
+        cv2.imwrite('item_found.jpg', out_frame)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
